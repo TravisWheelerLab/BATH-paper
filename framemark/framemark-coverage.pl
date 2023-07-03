bac6fc1fbe35472e3ee7539beff6fb4898d7d8b9
Genevieve Krause <gkrause@wentletrap.hpc.arizona.edu>
2023-07-03 14:53:59 -0600

    Improve framemark-coverage.pl to catch missed hits

--- framemark/framemark-coverage.pl
+++ framemark/framemark-coverage.pl
@@ -6,7 +6,7 @@ my $i;
 my $j;
 my $k;
 
-my $usage = "Usage: cat <outfile> | perl framemark-coverage.pl <posfile> \n";
+my $usage = "Usage: cat <outfile> | perl framemark-coverage.pl <posfile> <eval>\n";
 
 my $posfile = shift;
 my $eval_cuttoff = shift;
@@ -61,20 +61,16 @@ my $from;
 my $to;
 my $s;
 my $e;
-my $eval_over;
-my $long_over;
 my $total_over;
-my $eval_cover;
-my $long_cover;
 my $total_cover;
-my $tmp_long;
-my $tmp_over;
 my $true_length;
 
-printf("%20s	%11s	%10s	%10s	%10s	%10s	%10s	%10s	%10s\n", "HIT",  "TRUE-LENGTH", "EVAl-COVER", "EVAL-OVER", "LONG-COVER", "LONG-OVER", "TOT-COVER", "TOT-OVER", "CONTIGS");
+printf("%20s	%11s	%10s	%10s	%10s\n", "# HIT",  "TRUE-LENGTH", "COVERED", "OVER", "CONTIGS");
 for($i = 0; $i < @hit; $i++) {
 	$name = $hit[$i][-2];	
+	#if this is a new hit
     	unless(exists($matches{$name})) {
+		#if the hit passes the eval cuttoff
 		if($hit[$i][0] <= $eval_cuttoff) {
 			$matches{$name} = 1;
 			@froms          = ();
@@ -84,14 +80,10 @@ for($i = 0; $i < @hit; $i++) {
 			push(@froms, min($from,$to));
 			push(@tos,   max($from,$to));
 
-			#hits are already sorted by evalue so first hit has lowest evalue
-			$eval_cover  = $tos[0] - $froms[0] + 1;
-			$eval_over   = max($pos_starts{$name} - $froms[0], 0);
-			$eval_over  += max($pos_ends{$name}   - $tos[0],   0);
 			$true_length  = $pos_ends{$name} - $pos_starts{$name} + 1; 
 
 			for($j = $i+1; $j < @hit; $j++) {
-				if($hit[$j][6] eq $name && $hit[$j][0] <= 0.00001) {
+				if($hit[$j][6] eq $name && $hit[$j][0] <= $eval_cuttoff) {
 					$matches{$name} += 1;
 					$from = $hit[$j][2];
 					$to   = $hit[$j][3];
@@ -100,39 +92,6 @@ for($i = 0; $i < @hit; $i++) {
 				}
 			}
 	
-			#if only on hit it is the longest
-			if($matches{$name} == 1) {
-                                $long_cover  = $tos[0] - $froms[0] + 1;
-				$long_over   = $pos_starts{$name} - $froms[0];
-				$long_over  += $pos_ends{$name}   - $tos[0];
-				$long_cover -= $long_over;
-                        }
-			#if multiple hits find the hit with best coverage
-			else {
-				$long_cover  = $tos[0] - $froms[0] + 1;
-				$long_over   = $pos_starts{$name} - $froms[0];
-				$long_over  += $pos_ends{$name}   - $tos[0];
-				$long_cover -= $long_over;
-
-				for($k = 1; $k <  @froms; $k++) {
-					if(($tos[$k] - $froms[$k] + 1) > $long_cover) { #new hit is longer so might have better coverage
-						$tmp_long  = $tos[$k] - $froms[$k] + 1;
-						$tmp_over  = $pos_starts{$name} - $froms[$k];
-						$tmp_over += $pos_ends{$name}   - $tos[$k];
-						$tmp_long -= $tmp_over;
-						if($tmp_long > $long_cover) { #new hit has better coverage
-							$long_cover = $tmp_long;
-							$long_over  = $tmp_over;
-						} 
-						elsif($tmp_long == $long_cover) { #new hit has equal coverage pick the one with lower overage
-							if($long_over > $tmp_over) { #new hit has lover overage
-								$long_cover = $tmp_long;
-								$long_over  = $tmp_over;
-							}
-						}					
-					}
-				}
-			}
 
 			#find total overage														
 			$s          = min(@froms);
@@ -162,30 +121,25 @@ for($i = 0; $i < @hit; $i++) {
 			}
 			#if multiple hits consider all hits	
 			else {
-				#first remove hits which are entrily overlapped by other hits
+				#first remove hits which are entrily overlapped by a previous hit
 				#they cannot add to coverage and just complcate calcualtions
 				for($k = 1; $k <  @froms; $k++) {
-					if($froms[$k] >= $froms[$k-1] and $tos[$k] <= $tos[$k+1]) {
+					if($tos[$k] <= $tos[$k-1]) {
 						splice @froms, $k, 1;
 						splice @tos,   $k, 1;
 						$matches{$name} -= 1;
-					}
-					elsif($froms[$k-1] >= $froms[$k] and $tos[$k-1] <= $tos[$k]) {
-						splice @froms, $k-1, 1;
-						splice @tos,   $k-1, 1;
-						$matches{$name}   -= 1;	
 					}
 				}
 				#start with true hit length at total coverage then subtract all gaps
 				$total_cover  = $pos_ends{$name} - $pos_starts{$name} + 1;
 
-				#subtract gap any gap at begining and end using min start and 
+				#subtract  any gap at begining and end using min start and 
 				#max end variables from total overage calcuation
 				$total_cover -= $s - $pos_starts{$name}; 
 				$total_cover -= $pos_ends{$name} - $e;
 			
 				#if there are still multiple hits after removing total overlaps
-				#look for geps between hits and subtract from total coverage
+				#look for gaps between hits and subtract from total coverage
 				for($k = 1; $k <  @froms; $k++) {
 					if($tos[$k-1] < $froms[$k]) {
 						$total_cover -= $froms[$k] - $tos[$k-1];
@@ -193,7 +147,7 @@ for($i = 0; $i < @hit; $i++) {
 				}			
 			}
 
-			printf("%20s	%10d	%10d	%10d	%10d    %10d	%10d    %10d	%10d\n", $name, $true_length, $eval_cover, $eval_over, $long_cover, $long_over, $total_cover, $total_over, $matches{$name});
+			printf("%20s	%10d	%10d	%10d	%10d\n", $name, $true_length, $total_cover, $total_over, $matches{$name});
 		}
 	}
 }
