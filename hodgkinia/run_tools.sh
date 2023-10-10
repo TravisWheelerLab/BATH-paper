81be095cf2242e3c32e2f72b55aad692794815e8
Genevieve Krause <gkrause@wentletrap.hpc.arizona.edu>
2023-10-10 12:41:34 -0600

    Chage Hodkingia analysis to run bathsearch, tblastn and lastal instead of frahmmer, hmmsearcht and tblastn

--- hodgkinia/run_tools.sh
+++ hodgkinia/run_tools.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-# This Program runs three tools (frahmmer, hmmsearcht, and tblastn) to annotate the DNA of the Candidatus Hodgkinia genommes from 7 species of Magicicada using a set of protiens from Candidatus Hodgkinia in other cicada species
+# This Program runs three tools (bathsearch, tbalstn, and last) to annotate the DNA of the Candidatus Hodgkinia genommes from 7 species of Magicicada using a set of protiens from Candidatus Hodgkinia in other cicada species
 
 #stop the script if it attempts to use any unset variables
 set -o nounset
@@ -10,18 +10,15 @@ set -o errexit
 
 
 if [ $# -lt 4 ]; then
-    echo "ERROR: Your command line contains less than four arguments ($frahmmerdir, $hmmsearchtdir, $tblastndir, $easeldir),"
+    echo "ERROR: Your command line contains less than four arguments ($bathsearch_dir, $tblastn_dir, $lastal_dir, $easel_dir),"
     exit 1
 fi
 
 
 # Prepare query and target files
 
-# Make frahmmer hmm file from protien MSA
-$1/frahmmbuild --ct 4 query_files/query_hmms.fhmm query_files/query_msas.stk
-
-# Make hmmer hmm file from protien MSA
-$2/hmmbuild query_files/query_hmms.hmm query_files/query_msas.stk
+# Make BATH hmm file from protien MSA
+$1/bathbuild --ct 4 query_files/query_hmms.bhmm query_files/query_msas.stk
 
 # Make sequences from protien MSA for tblastn
 $4/miniapps/esl-reformat -o query_files/query_seqs.fa fasta query_files/query_msas.stk
@@ -29,9 +26,14 @@ $4/miniapps/esl-reformat -o query_files/query_seqs.fa fasta query_files/query_ms
 # Get list of query sequence lengths for use in full length hit anaylsis
 $4/miniapps/esl-seqstat -a query_files/query_seqs.fa > query_files/query_seqs.stat 
 
-# Make tbalstn target database files
-$3/makeblastdb -dbtype nucl -in target_files/MAGI_seqs -out target_files/MAGI_seqs
+# Make tblastn target database files
+$2/makeblastdb -dbtype nucl -in target_files/MAGI_seqs -out target_files/MAGI_seqs
 
+# Make last query database files
+$3/lastdb -P16 -q -c query_files/query_seqs.fa.db query_files/query_seqs.fa
+
+# Make last codon training file
+$3/last-train -P16 --codon query_files/query_seqs.fa.db target_files/MAGI_seqs  > target_files/MAGI_seqs.train
 
 # Create results directory to hold results files
 if ! [ -d "results_files" ]
@@ -41,13 +43,12 @@ fi
 
 # Run the tools 
 
-# Run frahmmer
-$1/frahmmer --ct 4 -o results_files/magi-frahmmer.out --tblout results_files/magi-frahmmer.tbl query_files/query_hmms.fhmm target_files/MAGI_seqs
-
-# Run hmmersearcht
-$2/hmmsearcht -c 4 -o results_files/magi-hmmsearcht.out --domtblout results_files/magi-hmmsearcht.tbl query_files/query_hmms.hmm target_files/MAGI_seqs
+# Run bathsearch
+$1/bathsearch --ct 4 -o results_files/magi-bath.out --tblout results_files/magi-bath.tbl query_files/query_hmms.bhmm target_files/MAGI_seqs
 
 # Run tblastn
-$3/tblastn -db_gencode 4 -outfmt 7 -out results_files/magi-tblastn.tbl -db target_files/MAGI_seqs -query query_files/query_seqs.fa
+$2/tblastn -db_gencode 4 -outfmt 7 -out results_files/magi-tblastn.tbl -db target_files/MAGI_seqs -query query_files/query_seqs.fa
 
+# Run lastal
+$3/lastal -p target_files/MAGI_seqs.train -K1 -fTAB query_files/query_seqs.fa.db target_files/MAGI_seqs > results_files/magi-last.tbl 
 
